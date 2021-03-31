fc86726ff7acc292bad8352e76eccced453f0c47
Tina <1098299287@qq.com>
2021-03-31 13:32:42 +0800

    修改hello world 界面

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <tina>
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Tina from "./components/Tina.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Tina,
+  },
+};
+</script>
---
+++ src/components/Tina.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>我的第一个组件</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
